7332dbd50bb3e71b842a2119dfbebe7f22fbe794
rakshiths-ms <rakshithsir9@gmail.com>
2026-03-16 18:06:48 +0530

    Refactor server.js for cleaner code structure

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
