eb5b0036a703c850d77d19078790557e039732c4
rakshiths-ms <rakshithsir9@gmail.com>
2026-03-16 21:16:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
+// connect mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/cakeDB")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 const server = http.createServer((req, res) => {
-  res.end("Server running");
+  res.end("Server running with MongoDB");
 });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
