458011325abb2b8c87c020cd15ea7d70716012a0
rakshiths-ms <rakshithsir9@gmail.com>
2026-03-16 20:41:05 +0530

    Switch to native HTTP server implementation
    Replaced express server with a native HTTP server.

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const http = require("http");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend running");
+const server = http.createServer((req, res) => {
+  res.end("Server running");
 });
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log("Server running on port 5000");
 });
